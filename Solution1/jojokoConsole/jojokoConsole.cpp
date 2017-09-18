a1ae654e9ce23648580945f91af631440dc5a888
bslama4 <bslama4@gmail.com>
2017-09-18 12:16:03 +0530

    no committ-ment

---
+++ Solution1/jojokoConsole/jojokoConsole.cpp
@@ -0,0 +1,13 @@
+// jojokoConsole.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+
+int main()
+{
+	std::cout << "oiley nai thiyo harey ni.." << std::endl;
+    return 0;
+}
+
